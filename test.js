cc9524d802eb21d54a0d8d73e0a3093b4ee8687b
Aakil Fernandes <aakilfernandes@gmail.com>
2018-01-18 16:30:29 -0500

    include amorph-hex in test

--- test.js
+++ test.js
@@ -4,20 +4,21 @@ const Amorph = require('./')
 const AmorphConverter = require('amorph-converter')
 const chai = require('chai')
 const intcoder = require('intcoder')
+const amorphHex = require('amorph-hex')
 
 chai.should()
 
 describe('Amorph', () => {
 
-  let intConverter
+  let amorphInt
   let amorph
 
   it('should create amorph', () => {
     amorph = new Amorph(new Uint8Array([1, 1]))
   })
 
-  it('should create hex converter', () => {
-    intConverter = new AmorphConverter((uint8Array) => {
+  it('should create amorphInt', () => {
+    amorphInt = new AmorphConverter((uint8Array) => {
       return intcoder.decode(uint8Array)
     }, (int) => {
       return intcoder.encode(int)
@@ -25,11 +26,11 @@ describe('Amorph', () => {
   })
 
   it('should amorph.to', () => {
-    amorph.to(intConverter).should.equal(257)
+    amorph.to(amorphInt).should.equal(257)
   })
 
   it('should amorph.from', () => {
-    Amorph.from(intConverter, 257).uint8Array.should.deep.equal(new Uint8Array([1, 1]))
+    Amorph.from(amorphInt, 257).uint8Array.should.deep.equal(new Uint8Array([1, 1]))
   })
 
   it('should .equals', () => {
@@ -37,15 +38,23 @@ describe('Amorph', () => {
   })
 
   it('should .as (-1)', () => {
-    amorph.as(intConverter, (int) => {
+    amorph.as(amorphInt, (int) => {
       return int - 1
     }).uint8Array.should.deep.equal(new Uint8Array([1, 0]))
   })
 
   it('should .as (-2)', () => {
-    amorph.as(intConverter, (int) => {
+    amorph.as(amorphInt, (int) => {
       return int - 2
     }).uint8Array.should.deep.equal(new Uint8Array([255]))
+  })
+
+  describe('amorph-hex', () => {
+    it('should would work', () => {
+      Amorph.from(amorphHex.unprefixed, '0101').as(amorphInt, (int) => {
+        return int - 2
+      }).to(amorphHex.prefixed).should.equal('0xff')
+    })
   })
 
 })
