bd2d7cefdf5b26ab1c3000b1cfed6f8f040d3e4a
Aakil Fernandes <aakilfernandes@gmail.com>
2018-01-22 17:47:07 -0500

    use defunction@5.1

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 const defunction = require('defunction')
+const getValidateInstanceOf = require('defunction/lib/validates/getInstanceOf')
+const getValidateTypeOf = require('defunction/lib/validates/getTypeOf')
+const getValidateConstructorNamed = require('defunction/lib/validates/getConstructorNamed')
+const validateAnything = require('defunction/lib/validates/anything')
 
-const Amorph = module.exports = defunction(['Uint8Array'], '*', function Amorph(uint8Array) {
+const validateUint8Array = getValidateInstanceOf(Uint8Array)
+const validateFunction = getValidateInstanceOf(Function)
+const validateAmorph = getValidateConstructorNamed('Amorph')
+const validateAmorphConverter = getValidateConstructorNamed('AmorphConverter')
+const validateUndefined = getValidateTypeOf('undefined')
+const validateString = getValidateTypeOf('string')
+const validateBoolean = getValidateTypeOf('boolean')
+
+const Amorph = module.exports = defunction([validateUint8Array], validateUndefined, function Amorph(uint8Array) {
   this.uint8Array = uint8Array
 })
 
-Amorph.prototype.toString = defunction([], 'string', function toString() {
+Amorph.prototype.toString = defunction([], validateString, function toString() {
   return `[Amorph uint8Array : [${this.uint8Array}]]`
 })
 
-Amorph.prototype.clone = defunction([], 'Amorph', function clone() {
+Amorph.prototype.clone = defunction([], validateAmorph, function clone() {
   return new Amorph(this.uint8Array.slice())
 })
 
-Amorph.prototype.to = defunction(['AmorphConverter'], '*', function to(converter) {
+Amorph.prototype.to = defunction([validateAmorphConverter], validateAnything, function to(converter) {
   return converter.to(this.uint8Array)
 })
 
-Amorph.prototype.equals = defunction(['Amorph'], 'boolean', function equals(amorph) {
+Amorph.prototype.equals = defunction([validateAmorph], validateBoolean, function equals(amorph) {
   if (amorph.uint8Array.length !== this.uint8Array.length) {
     return false
   }
@@ -28,11 +40,12 @@ Amorph.prototype.equals = defunction(['Amorph'], 'boolean', function equals(amor
   return true
 })
 
-Amorph.from = defunction(['AmorphConverter', '*'], 'Amorph', function from(converter, value) {
+// eslint-disable-next-line prefer-arrow-callback
+Amorph.from = defunction([validateAmorphConverter, validateAnything], validateAmorph, function from(converter, value) {
   return new Amorph(converter.from(value))
 })
 
-Amorph.prototype.as = defunction(['AmorphConverter', 'function'], 'Amorph', function as(converter, func) {
+Amorph.prototype.as = defunction([validateAmorphConverter, validateFunction], validateAmorph, function as(converter, func) {
   return Amorph.from(converter, func(this.to(converter)))
 })
 
